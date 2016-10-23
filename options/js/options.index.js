660147c588b42cbd3d42c326510b257e4f3d64d9
Jack Zhang <15jzhang@gmail.com>
2016-10-23 02:22:18 -0400

    Add form submission event handlers for options page

--- options/js/options.index.js
+++ options/js/options.index.js
@@ -1,2 +1,33 @@
 var $ = require('jquery');
 
+function handleFormSubmissions() {
+  function submitAccountSettings(firstName, lastName, email) {
+    console.log('submit account settings');
+  }
+
+  function activateWebsite(siteUrl, emailUsername, password) {
+    console.log('activate website');
+  }
+
+  $('.account-form').submit(function(e){
+     e.preventDefault();
+     var firstName = e.currentTarget['0'].value;
+     var lastName = e.currentTarget['1'].value;
+     var email = e.currentTarget['2'].value;
+
+     submitAccountSettings(firstName, lastName, email);
+  });
+
+  $('.website-form').submit(function(e){
+     e.preventDefault();
+     var siteUrl = e.currentTarget['0'].value;
+     var emailUsername = e.currentTarget['1'].value;
+     var password = e.currentTarget['2'].value;
+
+     activateWebsite(siteUrl, emailUsername, password);
+  });
+}
+
+$(document).ready(function() {
+  handleFormSubmissions();
+});
