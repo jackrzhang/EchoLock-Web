f90ebe9bc07b6411331000f069996c1e92f946ae
Jack Zhang <15jzhang@gmail.com>
2016-10-23 02:43:48 -0400

    Correct popup link to options

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ var webpack = require('webpack');
 var path = require('path');
 var OPTIONS_DIR = path.join(__dirname, 'options/');
 var POPUP_DIR = path.join(__dirname, 'popup/');
+var BACKGROUND_DIR = path.join(__dirname, 'background/');
 
 var optionsConfig = {
   name: 'options',
@@ -48,7 +49,30 @@ var popupConfig = {
   devtool: 'source-map'
 };
 
+var backgroundConfig = {
+  name: 'background',
+  output: {
+    path: path.join(BACKGROUND_DIR),
+    filename: 'background.bundle.js'
+  },
+  entry: path.join(BACKGROUND_DIR, 'background.index.js'),
+  module: {
+    loaders: [
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        loader: 'babel?presets[]=es2015'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js']
+  },
+  devtool: 'source-map'
+};
+
 module.exports = [
   optionsConfig,
-  popupConfig
+  popupConfig,
+  backgroundConfig
 ];
